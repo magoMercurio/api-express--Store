5a376c82a4242c0043de59e1bd9a3644eff2fc88
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-17 14:16:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('my app')
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('server en Express')
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('nueva ruta')
+})
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'producto 1',
+      price: 1000
+    },
+    {
+      name: 'producto 2',
+      price: 2000
+    }
+  ])
+})
+
+app.listen(port, () => {
+  console.log('mi port' + port)
+})
