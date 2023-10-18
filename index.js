002f5dd8c2efb63aa5b90d86bf0e2dd31129ec05
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-18 14:17:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,47 @@ app.get('/products', (req, res) => {
   ])
 })
 
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'producto 2',
+    price: 2000,
+  });
+});
+
+app.get('/categories',(req,res)=>{
+  const {categoryId}= req.params
+  res.json([
+    {
+      categoryId,
+      category: 'Food',
+      products: []
+    },
+    {
+      categoryId,
+      category: 'Games',
+      products: []
+    },
+    {
+      categoryId,
+      category: 'clothes',
+      products: []
+    },
+  ])
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  })
+})
+
+app.get('/categories/')
+
+
 app.listen(port, () => {
   console.log('mi port' + port)
 })
