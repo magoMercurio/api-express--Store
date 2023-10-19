7628c735a61991522eeaf765cdd71a3e3770ec0b
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-19 12:24:42 -0500

    Update index.js
    add get for users and orders

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+// ruta raiz
 app.get('/', (req, res) => {
     res.send('server en Express')
 })
 
+//ejemplo, borrar al final
 app.get('/nueva-ruta', (req, res) => {
   res.send('nueva ruta')
 })
 
+// ruta para obtener productos
 app.get('/products', (req, res) => {
   res.json([
     {
@@ -23,6 +26,7 @@ app.get('/products', (req, res) => {
   ])
 })
 
+// ruta para obtener productos por id
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   res.json({
@@ -32,6 +36,7 @@ app.get('/products/:id', (req, res) => {
   });
 });
 
+// ruta para obtener categorias
 app.get('/categories',(req,res)=>{
   const {categoryId}= req.params
   res.json([
@@ -53,6 +58,7 @@ app.get('/categories',(req,res)=>{
   ])
 })
 
+// ruta para obtener categorias por id
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
   res.json({
@@ -61,30 +67,55 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
   })
 })
 
+// ruta para obtener ordenes
+app.get('/orders', (req, res) => {
+  res.json ([
+    {
+      name: 'order 1',
+      price: 1000,
+    },
+    {
+      name: 'order 2',
+      price: 2000,
+    },
+    {
+      name: 'order 3',
+      price: 3000,
+    }
+  ])
+})
+
+// ruta para obtener ordenes por id
+app.get('/orders/:orderId', (req, res) => {
+  const { orderId } = req.params
+  res.json({
+    orderId,
+    name: 'order 1',
+    price: 1000,
+  })
+})
+
 app.get('/users', (req, res) => {
   res.json ([
     {
-      id,
       name: 'user 1',
       age: 20,
     },
     {
-      id,
       name: 'user 2',
       age: 30,
     },
     {
-      id,
       name: 'user 3',
       age: 40,
     }
   ])
 })
 
-app.get('/users/:id', (req, res) => {
-  const { id } = req.params
+app.get('/users/:userId', (req, res) => {
+  const { userId } = req.params
   res.json({
-    id,
+    userId,
     name: 'user 1',
     age: 20,
   })
